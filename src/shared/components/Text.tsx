98fe9ebaec2ca8671cd34c4b6d9d0195837008fb
obb8923 <obb8923@naver.com>
2025-06-22 21:51:14 +0900

    fix: Text fontStyle 가변 적용

--- src/shared/components/Text.tsx
+++ src/shared/components/Text.tsx
@@ -6,12 +6,36 @@ export type TextProps = {
     style?: TextStyle | TextStyle[];
     numberOfLines?: number;
   };
+const fontStyle = (type: 'regular' | 'semibold' | 'extrabold' | 'black'): TextStyle => {
+  switch(type){
+    case 'regular':
+      return {
+        fontFamily: 'Pretendard-Regular',
+      };
+    case 'semibold':
+      return {
+        fontFamily: 'Pretendard-SemiBold',
+      };
+    case 'extrabold':
+      return {
+        fontFamily: 'Pretendard-ExtraBold',
+      };
+    case 'black':
+      return {
+        fontFamily: 'Pretendard-Black',
+      };
+    default:
+      return {
+        fontFamily: 'Pretendard-Regular',
+      };
+  }
+}
 export const Text = ({text, type='regular', ...props}: TextProps) => {
   return (
     <RNText 
     {...props}
-      className={`font-p-${type} ${props.className}`}
-      style={[props.style]}
+      className={props.className}
+      style={[fontStyle(type), props.style]}
       numberOfLines={props.numberOfLines}>
       {text}
     </RNText>
