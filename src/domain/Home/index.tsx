2912dfbd67c29db39010b3dfbcab5786dea5385f
obb8923 <obb8923@naver.com>
2025-06-22 21:19:53 +0900

    font

--- src/domain/Home/index.tsx
+++ src/domain/Home/index.tsx
@@ -6,7 +6,10 @@ export function HomeScreen() {
   return (
     <View className='flex-1 bg-green-500 justify-center items-center'>
       <Right width={48} height={48} color="white" />
-      <Text className='text-white text-xl mt-4'>Home Screen</Text>
+      <Text className='text-white text-xl mt-4 font-p'>Pretendard Regular</Text>
+      <Text className='text-white text-xl mt-4 font-p-semibold'>Pretendard SemiBold</Text>
+      <Text className='text-white text-xl mt-4 font-p-extrabold'>Pretendard ExtraBold</Text>
+      <Text className='text-white text-xl mt-4 font-p-black'>Pretendard Black</Text>
     </View>
   );
 }
