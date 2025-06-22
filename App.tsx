ad838fcb99c2ec9e8df0a9be931665fab59b7e9f
obb8923 <obb8923@naver.com>
2025-06-22 20:44:51 +0900

    amenity

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import { StatusBar,View } from 'react-native';
+import React from 'react';
+import { StatusBar} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { HomeScreen } from './src/domain/Home';
+
+type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-
   return (
-    <View style={{flex:1,backgroundColor:'green'}}>
-      <StatusBar/>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{flex:1}} edges={[ 'left', 'right']} >
+              <NavigationContainer>
+                <StatusBar barStyle="dark-content" translucent={true}/>
+                <Stack.Navigator screenOptions={{headerShown:false}}>
+                  <Stack.Screen name="Home" component={HomeScreen} />
+                </Stack.Navigator>
+              </NavigationContainer>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
